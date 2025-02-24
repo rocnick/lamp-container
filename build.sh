dfe120af9fafdef2012c45e7f10cae231e360691
Nick Snyder <nick@liftbridgestudio.com>
2025-02-24 10:35:07 -0500

    platform flag

--- build.sh
+++ build.sh
@@ -3,4 +3,8 @@
 # If you need platform specific dependencies,
 # add the flag --platform=linux/amd64 (or which ever architecture you require)
 
+# docker build --platform=linux/amd64 -t web-server -f Dockerfile .
+
+# else
+
 docker build -t web-server -f Dockerfile .
